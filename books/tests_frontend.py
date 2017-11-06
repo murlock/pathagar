c6592371237b6999cab17d61adfaf3f4a8d3a3da
Michael Bonfils <murlock42@gmail.com>
2017-11-06 17:22:06 +0100

    Add Selenium tests for simple search
    - no match
    - simple match on bookname

--- books/tests_frontend.py
+++ books/tests_frontend.py
@@ -1,25 +1,23 @@
-#!/u/bin/env python
-
 import os
-import time
+from time import sleep
 import unittest
+import re
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import TimeoutException
 
-from django.test import TestCase, LiveServerTestCase
-#from testing import StaticLiveServerTestCase
+from django.test import TestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.core.management import call_command, CommandError
+from django.urls import reverse_lazy
+
 
 from django.contrib.auth.models import User
 
-# from books.epub import Epub
 from books.models import Author
 
-#BASE = "http://localhost:8000"
-
-class PathagarBook(StaticLiveServerTestCase): #LiveServerTestCase):
+class PathagarBook(StaticLiveServerTestCase):
     ADMIN_USER = 'admin'
     ADMIN_PASS = 'pass'
 
@@ -37,17 +35,21 @@ class PathagarBook(StaticLiveServerTestCase): #LiveServerTestCase):
         anonymous = Author(a_author=self.AUTHOR)
         anonymous.save()
 
-    """
-    def test_01_check_protected_url(self):
+    def tearDown(self):
+        sleep(5)
+        self.driver.close()
 
-    def test_02_
+    def wait_url(self, url, timeout=5, regex=False):
+        while timeout > 0:
+            timeout -= 0.1
+            if regex and re.match(url, self.driver.current_url):
+                return
+            elif url == self.driver.current_url:
+                return
+            sleep(0.1)
+        raise TimeoutException("URL %s not reached (current URL: %s)" % (url, self.driver.current_url))
 
-    def test_03_
-
-    def test_04_invalid_login(self):
-    """
-
-    def test_05_login(self):
+    def test_03_login(self):
         drv = self.driver
         drv.get(self.live_server_url)
 
@@ -63,18 +65,15 @@ class PathagarBook(StaticLiveServerTestCase): #LiveServerTestCase):
         elem.send_keys(self.ADMIN_PASS)
         elem.send_keys(Keys.RETURN)
 
-        time.sleep(1)
+        # sleep(1)
+        self.wait_url(url)
 
         self.assertEqual(url, drv.current_url)
 
-        # time.sleep(1)
+        # sleep(1)
 
         elem = drv.find_elements_by_xpath("//*[contains(text(), 'Add Book')]")[0]
         elem.send_keys(Keys.RETURN)
-
-        # it assume that Author anonymous exist
-
-        # time.sleep(1)
 
         fullpath = os.path.abspath("./examples/The Dunwich Horror.epub")
         self.assertTrue(os.path.isfile(fullpath))
@@ -97,7 +96,28 @@ class PathagarBook(StaticLiveServerTestCase): #LiveServerTestCase):
         elem = drv.find_element_by_xpath("//input[@value='Add']")
         elem.send_keys(Keys.RETURN)
 
-        # XXX Check value
+        self.wait_url(r'^http://[a-zA-Z0-9:]+/book/\d+/view$', regex=True)
 
-    def tearDown(self):
-        self.driver.close()
+
+        latest_url = str(self.live_server_url) + str(reverse_lazy("latest"))
+        drv.get(latest_url)
+        self.wait_url(latest_url)
+
+        elem = drv.find_elements_by_css_selector("h1.bookname > a")
+        self.assertEqual(elem[0].text, "The Dunwich Horror")
+
+        # Launch a simple search (from books)
+        for term, results in [("xxxxxxx", []), ("Dunwich", ["The Dunwich Horror"])]:
+            print(term)
+            elem = drv.find_element_by_id("search")
+            elem.clear()
+            elem.send_keys(term)
+            elem.submit()
+
+            self.wait_url(r'.*?q=' + term, regex=True)
+
+            founds = drv.find_elements_by_css_selector("h1.bookname > a")
+            self.assertEqual(len(founds), len(results))
+            if results:
+                names = [x.text for x in founds]
+                self.assertEqual(names, results)
